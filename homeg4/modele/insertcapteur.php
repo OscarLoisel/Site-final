9cae75f74e35c38c9e7ab79d071406680463f50a
Jifllaume <guillaumpasque@gmail.com>
2017-01-13 12:06:56 +0100

    I'm Back Guys !!!
    :+1:

--- homeg4/modele/insertcapteur.php
+++ homeg4/modele/insertcapteur.php
@@ -2,17 +2,33 @@
 	require("connexion_base.php");
 
 
-	function insertcapteur($bdd, $id_piece, $capteur, $type)
+	function checkCapteur($bdd, $n_serie)
 	{
-		$reponse = $bdd -> prepare('INSERT INTO capteurs(id_piece, capteur, type) VALUES (?, ?, ?)');
-		$reponse -> execute(array($id_piece, $capteur, $type));
+		$reponse = $bdd -> prepare('SELECT * FROM produit WHERE n_serie = ?');
+		$reponse -> execute(array($n_serie));
 		return $reponse;
 	}
 
-	function insertcapteur2($bdd, $n_serie,)
+	function idNewPiece($bdd, $id_utilisateur)
+	{
+		$reponse = $bdd -> prepare('SELECT * FROM pieces WHERE id_utilisateur = ? ORDER BY DESC')
+		$reponse -> execute(array($n_serie));
+		return $reponse;
+	}
+
+
+	function insertcapteur($bdd, $id_piece, $type, $n_serie)
+	{
+		$reponse = $bdd -> prepare('INSERT INTO capteurs(id_piece, type, n_serie) VALUES (?, ?, ?)');
+		$reponse -> execute(array($id_piece, $type, $n_serie));
+		return $reponse;
+	}
+
+	function insertcapteur2($bdd, $n_serie)
 	{
 		$reponse = $bdd -> prepare('INSERT INTO capteurs(n_serie) VALUES (?)');
 		$reponse -> execute(array($n_serie,));
 		return $reponse;
 	}
+
 ?>
