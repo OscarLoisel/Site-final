bc7265288169443992ca0b0a625c74b9a093a4f8
HomeYann <k2092324@mvrht.com>
2017-01-16 14:48:18 +0100

    Sav le vrai
    Description utile

--- homeg4/vue/sav.php
+++ homeg4/vue/sav.php
@@ -1,74 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Homeg4r</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="../style/style.css">
-</head>
-<body>
+<?php
 
-<div class="page">
+    $entete = entete("sav");
+    $aside = aside("sav");
+    $contenu = '<div class="titre_bienvenue">';
+    $contenu .= "<h1> Nous contactez: </h1>";
+	$contenu .="<p> En cas de problème notre service après vente est à votre disposition. </p>";
+	$contenu .="<p> Nous vous invitions cependant au préalable à consulter notre FAQ sur le forum.</p>";
+	$contenu .="<p> Sinon, vous pouvez nous contactez sur facebook ou twitter </p>";
+	$contenu .="<p> Salaam! </p>";
+    $contenu .= '</div>';
+    $pied = pied();
 
-<?php  
-	include("../controleur/menu.php");
+    include 'gabarit.php';
 ?>
-
-
-<section id="home">
-	<aside>
-
-		<div id="tdb">
-				<table id="tableau_tdb">
-
-					<tr>
-						<td colspan="2">Tableau de bord</td>
-					</tr>
-
-					<tr>
-						<td class="critere">Température</td>
-						<td class="critere">Humidité</td>
-					</tr>
-
-					<tr>
-						<td class="critere">Alarme</td>
-						<td class="critere">Caméras</td>
-					</tr>
-					
-				</table>
-				<label class="switch">
-  						<input type="checkbox">
-  						<div class="slider"></div>
-				</label>
-		</div>
-
-		<div id="menu_capteurs">
-			<div id="liste_capteurs">
-				<ul>
-					<a href="lampes.php"><li>Lampes</li></a>
-					<a href="#"><li>Chauffage</li></a>
-					<a href="#"><li>Volets</li></a>
-					<a href="#"><li>Alarme</li></a>
-					<a href="#"><li>Caméras</li></a>
-				</ul>
-			</div> 
-			
-		</div>
-
-	</aside>
-	
-	
-	
-	<div id="sav">
-		<h1> Nous contactez: </h1>
-		<p> En cas de problème notre service après vente est à votre disposition. </p>
-		<p> Nous vous invitions cependant au préalable à consulter notre FAQ sur le forum.</p>
-		<p> Sinon, vous pouvez nous contactez 08666zenumberovzeBeast </p>
-	</div>
-
-
-</section>
-	
-</div>
-
-</body>
-</html>
