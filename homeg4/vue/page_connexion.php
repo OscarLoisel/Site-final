323430a66993dd3674917dbaf31b80cea6921e83
jeremyhuang96 <jeremy.huang@isep.fr>
2017-01-16 17:08:42 +0100

    rds

--- homeg4/vue/page_connexion.php
+++ homeg4/vue/page_connexion.php
@@ -1,9 +1,8 @@
 <?php
-
     $entete = enteteConnexion();
     $aside = asideHorsConnexion();
     $contenu = '<div class="titre_bienvenue">';
-    $contenu .= "<h1> Bienvenue chez vous où que vous soyez !</h1> ";
+    $contenu .= "<h1>Bienvenue chez vous <br /> où que vous soyez !</h1> ";
     $contenu .= '</div>';
     $pied = pied();
 
