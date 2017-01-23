5b8bd9357171beaa3e0b56349c6c1e0f770a0a1d
LouisSlu <saliou@MacBook-Pro-de-saliou.local>
2017-01-23 14:58:39 +0100

    reglage

--- homeg4/modele/affichage_reglage_capteur.php
+++ homeg4/modele/affichage_reglage_capteur.php
@@ -3,15 +3,15 @@ require("modele/connexion_base.php");
 
 function affichage_table_capteur($bdd, $id_piece)
 {
-	$reponse = $bdd ->prepare('SELECT * FROM capteur WHERE id_piece = ?');
+	$reponse = $bdd ->prepare('SELECT * FROM capteurs WHERE id_piece = ?');
 	$reponse -> execute(array($id_piece));
 	return $reponse;
 }
 
 function affichage_table_donnees($bdd, $id_capteur)
 {
-	$reponse = $bdd ->prepare('SELECT * FROM donnees WHERE id_capteur = ?');
-	$reponse -> execute(array($id_capteur));
-	return $reponse;
+	$req = $bdd ->prepare('SELECT * FROM donnees WHERE id_capteur = ?');
+	$req -> execute(array($id_capteur));
+	return $req;
 }
 ?>
--- homeg4/vue/page_reglage_capteur.php
+++ homeg4/vue/page_reglage_capteur.php
@@ -1,8 +1,17 @@
 <?php
+include("modele/affichage_reglage_capteur.php");
 
 $entete = entete("home");
 $aside = aside("sav");
 $contenu = '<h1>Réglages</h1>';
+
+$reponse = affichage_table_capteur($bdd, $id_piece);
+$donnees = $reponse->fetch();
+$id_capteur = $donnees['id'];
+
+$req = affichage_table_donnees($bdd, $id_capteur);
+$data = $req->fetch();
+
 $contenu .= listecapteurs();
 
 // NOM_CAPTEURS - DONNEES DU CAPTEURS - BATTERIE DU CAPTEURS - VERT - ORANGE - ROUGE
