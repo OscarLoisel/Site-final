3e8405f9e66467dacfc58bd0e229cbef41634a53
LouisSlu <saliou@MacBook-Pro-de-saliou.local>
2017-01-23 16:26:45 +0100

    oui c'est beau

--- homeg4/vue/page_reglage_capteur.php
+++ homeg4/vue/page_reglage_capteur.php
@@ -4,56 +4,55 @@ include_once("modele/affichage_reglage_capteur.php");
 $entete = entete("home");
 $aside = aside("sav");
 $contenu = '<h1>Réglages</h1>';
+$contenu .= listecapteurs();
 
 $reponse = affichage_table_capteur($bdd, $id_piece);
-$donnees = $reponse->fetch();
-$id_capteur = $donnees['id'];
-
-echo $donnees['n_tram'];
-echo $donnees['n_serie'];
-
-$req = affichage_table_donnees($bdd, $id_capteur);
-$data = $req->fetch();
-
-/*$contenu .= listecapteurs();
-$contenu .='<tr>';
-$contenu .='<td>';
-$contenu .=	$donnees['n_tram']; // TABLE CAPTEUR 	
-$contenu .='</td>';
-$contenu .='<td>';
-$contenu .= $donnees['n_serie']; //TABLE CAPTEUR
-$contenu .='</td>';
-$contenu .='<td>';
-$contenu .= $data['valeur']; //TABLE DONNEES
-$contenu .='</td>';
-$contenu .='<td>';
-$contenu .= $donnees['batterie']; //TABLE CAPTEUR
-$contenu .='</td>';
-$contenu .='<td>';
-$contenu .= $data['date']; //TABLE DONNEES
-$contenu .='</td>';
-$contenu .='<td>';
-
-if ($donnees['etat'] == 1) //TABLE CAPTEUR
+while ($donnees = $reponse->fetch()) 
 {
-	 $contenu .= "vert";
-}
-else
-{
-	$contenu .= "rouge";
-}
+	$id_capteur = $donnees['id'];
 
-$contenu .='</td>';
+	echo $donnees['n_tram'].'<br>';
+	echo $donnees['n_serie'].'<br>';
+
+	$req = affichage_table_donnees($bdd, $id_capteur);
+	$data = $req->fetch();
+	
+	$contenu .= '<tr>';
+	$contenu .= '<td>';
+	$contenu .=	$donnees['n_tram']; // TABLE CAPTEUR 	
+	$contenu .= '</td>';
+	$contenu .= '<td>';
+	$contenu .= $donnees['n_serie']; //TABLE CAPTEUR
+	$contenu .= '</td>';
+	$contenu .= '<td>';
+	$contenu .= $data['valeur']; //TABLE DONNEES
+	$contenu .= '</td>';
+	$contenu .= '<td>';
+	//$contenu .= $donnees['batterie']; //TABLE CAPTEUR
+	$contenu .= '</td>';
+	$contenu .= '<td>';
+	$contenu .= $data['date']; //TABLE DONNEES
+	$contenu .= '</td>';
+	$contenu .= '<td>';
+	$contenu .= '<td>';
+	$contenu .= '</td>';
+	$contenu .= '<td>';
+
+	if ($donnees['etat'] == 1) //TABLE CAPTEUR
+	{
+		$contenu .= "vert";
+	}
+	else
+	{
+		$contenu .= "rouge";
+	}
+
+	$contenu .='</td>';
+	$contenu .='</tr>';
+	
+	
+}
 $contenu .='</table>';
-$contenu .=
-$contenu .=
-$contenu .=
-$contenu .=
-$contenu .=
-$contenu .=
-$contenu .=
-$contenu .=
-$contenu .= */
 
 // NOM_CAPTEURS - DONNEES DU CAPTEURS - BATTERIE DU CAPTEURS - VERT - ORANGE - ROUGE
 
