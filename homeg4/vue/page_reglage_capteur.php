ba018e4768c9b7e5d436bb45aa6dbe58f4e56941
LouisSlu <saliou@MacBook-Pro-de-saliou.local>
2017-01-23 15:19:32 +0100

    kdb

--- homeg4/vue/page_reglage_capteur.php
+++ homeg4/vue/page_reglage_capteur.php
@@ -1,6 +1,6 @@
 <?php
-include("modele/affichage_reglage_capteur.php");
-
+include_once("modele/affichage_reglage_capteur.php");
+include_once("vue/commun.php");
 $entete = entete("home");
 $aside = aside("sav");
 $contenu = '<h1>Réglages</h1>';
@@ -8,6 +8,9 @@ $contenu = '<h1>Réglages</h1>';
 $reponse = affichage_table_capteur($bdd, $id_piece);
 $donnees = $reponse->fetch();
 $id_capteur = $donnees['id'];
+
+echo $donnees['n_tram'];
+echo $donnees['n_serie'];
 
 $req = affichage_table_donnees($bdd, $id_capteur);
 $data = $req->fetch();
