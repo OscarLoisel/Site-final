c1cfc58a44f323b2be2b2bfe4a465967cee1e612
LouisSlu <saliou@MacBook-Pro-de-saliou.local>
2017-01-26 11:46:47 +0100

    mourrai

--- homeg4/index.php
+++ homeg4/index.php
@@ -156,6 +156,8 @@
         $tram = htmlspecialchars($_POST['tram']);
         if (!empty($tram)) 
         {
+            $id_utilisateur = intval($_SESSION['id']);
+
             $type_tram = substr($tram, 0, 1); //1 - TYPE DE TRAM UTILISÉ - toujours la premiere tram
             $objet = substr($tram, 1, 4); //4 - NUMERO D'EQUIPE - ne change jamais
             $requete = substr($tram, 5, 1); //1 - IDENTIFIE LA COMMANDE - modifie l'etat du capteur
@@ -164,7 +166,7 @@
             $valeur_capteur_hex = substr($tram, 9, 4); //4 - VALEUR DU CAPTEUR
             $date = substr($tram, 13, 4); // 4 - DATE
 
-            insertdonnees($bdd, $valeur_capteur_hex, $date, $numero);
+            insertdonnees($bdd, $valeur_capteur_hex, $date, $numero, $id_utilisateur);
         }
         else
         {
