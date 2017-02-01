6ed9129ed58e14bdb409956bf04990ef2dd29a7a
Jifllaume <guillaumpasque@gmail.com>
2017-02-01 14:24:51 +0100

    bug avec le 1

--- homeg4/JS/js.js
+++ homeg4/JS/js.js
@@ -23,27 +23,24 @@ function test()
 function test2()
 {
 	var x = document.getElementById('nb_capteur').value;
-	if(x == 1)
-	{
-		document.getElementById('test3').innerHTML = '';
-	}
+	
 	if(x == 2)
 	{
 		document.getElementById('test3').innerHTML = '<table class="paraPieceTest"><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><tr><td><input type="submit" value="Ajouter" name="formparapieces"></td></tr></table>';
 	}
-	if(x == 3)
+	else if(x == 3)
 	{
 		document.getElementById('test3').innerHTML = '<table class="paraPieceTest"><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br /><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><tr><td><input type="submit" value="Ajouter" name="formparapieces"></td></tr></table>';
 		//document.getElementById('test3').innerHTML += '<tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr></table>';
 	}
-	if(x == 4)
-	{
+	else if(x == 4)
+		{
 		document.getElementById('test3').innerHTML = '<table class="paraPieceTest"><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br /><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br /><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><tr><td><input type="submit" value="Ajouter" name="formparapieces"></td></tr></table>';
 		//document.getElementById('test3').innerHTML += '<tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br />';
 		//document.getElementById('test3').innerHTML += '<tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr></table>';
-	}	
+		}	
 	
-	if(x == 5)
+	else if(x == 5)
 		{
 			document.getElementById('test3').innerHTML = '<table class="paraPieceTest"><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br /><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br /><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br /><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><tr><td><input type="submit" value="Ajouter" name="formparapieces"></td></tr></table>';
 			/*document.getElementById('test3').innerHTML += '<tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br />';
@@ -51,13 +48,18 @@ function test2()
 			document.getElementById('test3').innerHTML += '<tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr></table>';*/
 		}
 	
-	if(x == 6)
+	else if(x == 6)
 		{
 			document.getElementById('test3').innerHTML = '<table class="paraPieceTest"><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br /><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br /><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br /><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br /><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><tr><td><input type="submit" value="Ajouter" name="formparapieces"></td></tr></table>';
 			/*document.getElementById('test3').innerHTML += '<tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br />';
 			document.getElementById('test3').innerHTML += '<tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br />';
 			document.getElementById('test3').innerHTML += '<tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><br />';
 			document.getElementById('test3').innerHTML += '<tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr></table>';*/
+		}
+	else
+		{
+			alert(x);
+			document.getElementById('test3').innerHTML = '<table class="paraPieceTest"><tr><td><label for="ajout_capteur">Ajouter un capteur :</label></td><td><input type="text" name="ajout_capteur" id="ajout_capteur" placeholder="n° de série"></td></tr><tr><td><input type="submit" value="Ajouter" name="formparapieces"></td></tr></table>';
 		}
 	
 
