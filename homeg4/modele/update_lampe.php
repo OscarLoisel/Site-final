7077f639012991108442364edea478fedadaab9c
LouisSlu <saliou@MacBook-Pro-de-saliou-2.local>
2017-02-02 14:14:53 +0100

    efi

--- homeg4/modele/update_lampe.php
+++ homeg4/modele/update_lampe.php
@@ -1,7 +1,7 @@
 <?php
 include("modele/connexion_base.php");
 
-function read_lampe($bdd)
+/*function read_lampe($bdd)
 {
 	$reponse = $bdd -> prepare('SELECT capteurs.id,type, id_utilisateur 
 		FROM capteurs,pieces 
@@ -21,5 +21,11 @@ function update_lampe_off ($bbd, $id_capteur)
 {
 	$reponse = $bbd -> prepare('UPDATE capteurs SET type = 0 WHERE id = ?');
 	$reponse -> execute(array($id_capteur));
+}*/
+
+function insert_scenario ($bdd, $nom_scenario, $date_debut, $date_fin, $heure_debut, $heure_fin, $action)
+{
+	$reponse = $bdd -> prepare('INSERT INTO scenarios(nom_scenario, date_debut, date_fin, heure_debut, $action) VALUES (?, ?, ?, ?, ?)');
+	$reponse -> execute($nom_scenario, $date_debut, $date_fin, $heure_debut, $heure_fin, $action);
 }
 ?>
