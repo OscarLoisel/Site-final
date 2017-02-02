04fca668fc95e8cfdce35668b875c69232b85d82
LouisSlu <saliou@MacBook-Pro-de-saliou-2.local>
2017-02-02 11:43:40 +0100

    mourrai

--- homeg4/Controleur/update_lampe.php
+++ homeg4/Controleur/update_lampe.php
@@ -1,29 +1,48 @@
 <?php
-include_once('modele/update_lampe.php');
+/*include_once('modele/update_lampe.php');
 
 if (isset($_POST['rafraichir'])) 
+{
+	if (isset($_POST['onoffswitch'])) 
     {
-    	if (isset($_POST['onoffswitch'])) 
-	    {
-	    	
-	    	$reponse = read_lampe($bdd);
-	    	while ($data = $reponse-> fetch()) 
-	    	{
-	    		$id_capteur = $data['capteurs.id'];
-	    		
-	    		$reponse = update_lampe_on($bdd, $id_capteur);
-	    	}
-	    }
-	    else
-	    {
-	    	$reponse = read_lampe($bdd);
-	    	while ($data = $reponse-> fetch()) 
-	    	{
-	    		$id_capteur = $data['capteurs.id'];
-	    		
-	    		$reponse = update_lampe_off($bdd, $id_capteur);
-	    	}
-	    }
+    	
+    	$reponse = read_lampe($bdd);
+    	while ($data = $reponse-> fetch()) 
+    	{
+    		$id_capteur = $data['capteurs.id'];
+    		
+    		$reponse = update_lampe_on($bdd, $id_capteur);
+    	}
     }
+    else
+    {
+    	$reponse = read_lampe($bdd);
+    	while ($data = $reponse-> fetch()) 
+    	{
+    		$id_capteur = $data['capteurs.id'];
+    		
+    		$reponse = update_lampe_off($bdd, $id_capteur);
+    	}
+    }
+}*/
+if (isset($_POST['formscenario'])) 
+{
+	$date_debut = htmlspecialchars($_POST['date_debut']);
+	$date_fin = htmlspecialchars($_POST['date_fin']);
+	if (!empty($date_debut)) 
+	{
+		echo "date_debut";
+		if (!empty($date_fin)) 
+		{
+			echo "date_fin";
+			if (isset($_POST['choixh']))  
+			{
+				//$choix_h = $_POST['choixh'];
+				//echo $choix_h;
+				echo "string";
+			}
+		}
+	}
+}
 
 ?>
