0afcfb3d290a52d8758142f5134eefddfb943f42
LouisSlu <saliou@macbook-pro-de-saliou-2.home>
2017-02-05 17:49:13 +0100

    admin

--- homeg4/vue/gestion_utilisateur.php
+++ homeg4/vue/gestion_utilisateur.php
@@ -3,28 +3,41 @@
 
 	$entete = enteteAdmin("utilisateur");
 	$aside = asideHorsConnexion();
-	$contenu = '<div id="titre">';
+	$contenu ='<div id="table_gestion">';
+	$contenu .= '<div id="titre">';
 	$contenu .= '<h2>Gestion des membres</h2>';
 	$contenu .= '</div>';
 	$reponse = $bdd->query('SELECT * FROM utilisateur ORDER BY id DESC');
-	$contenu .='<ul>';
+	$contenu .='<table id="utilisateur_table">';
 	while ($utilisateur = $reponse ->fetch()) 
 	{
+		$contenu .='<tr>';
+		$contenu .='<td>';
 		$id_mbr = $utilisateur['id'];
-		$contenu .= '<li>';
 		$contenu .= $utilisateur['id'];  
-		$contenu .= ' : ';
-		$contenu .= $utilisateur['identifiant'];
-		if ($utilisateur['confirme'] == 0) 
+		$contenu .='</td>';
+		$contenu .='<td>';
+		$contenu .= $utilisateur['mail'];
+		$contenu .='</td>';
+		/*if ($utilisateur['confirme'] == 0) 
 		{
+			$contenu .='<td>';	
 			$contenu .= '<a href="index.php?cible=confirme&amp;confirme='.$id_mbr.'">Confirmer</a>';
+			$contenu .='</td>';
 		}
-		$contenu .= ' - ';
+		else
+		{
+			$contenu .='<td>';
+			$contenu .='</td>';
+		}*/
+		$contenu .='<td>';
 		$contenu .= '<a href="index.php?cible=delete&amp;delete='.$id_mbr.'"> Supprimer</a>';
-		$contenu .= '</li>';
+		$contenu .='</td>';
+		$contenu .='</tr>';
 
 	}
-	$contenu .='</ul>';
+	$contenu .='</table>';
+	$contenu .='</div>';
 	$pied = pied();
 
 	include'gabarit.php';
