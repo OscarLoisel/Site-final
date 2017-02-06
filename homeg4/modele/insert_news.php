7841874a6f5a05f0fec4547fd4e7cc98602e5ba6
HomeYann <k2092324@mvrht.com>
2017-02-06 16:15:13 +0100

    B0l0ss, l'attaque finale
    PARTIE 4!! (on va dépasser l'age de glace à ce rythme...)

--- homeg4/modele/insert_news.php
+++ homeg4/modele/insert_news.php
@@ -1,7 +1,7 @@
 <?php
 function insert_newss($bdd, $titre, $message)
 {
-$req = $bdd->prepare('INSERT INTO news (titre, message) VALUES(?, ?, ?)');
+$req = $bdd->prepare('INSERT INTO news (titre, message) VALUES(?, ?)');
 $req->execute(array($titre, $message));
 
 }
