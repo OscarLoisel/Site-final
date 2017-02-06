3568b7a1a0840afeddff2e88b34f41ebf9a82264
LouisSlu <saliou@MacBook-Pro-de-saliou-2.local>
2017-02-06 12:18:11 +0100

    ca marche

--- homeg4/modele/insertion_tram.php
+++ homeg4/modele/insertion_tram.php
@@ -9,7 +9,7 @@ function readutilisateurs($bdd)
 
 function insertdonnees($bdd, $valeur_capteur_hex, $date, $numero, $id_utilisateur)
 {
-	$reponse = $bdd-> prepare("INSERT INTO donnees(valeur,date_capteur,id_capteur, id_utilisateur) VALUES(?, ?, ?, ?) WHERE id_utilisateur = ?");
+	$reponse = $bdd-> prepare("INSERT INTO donnees_reçu(valeur,date_capteur,id_capteur, id_utilisateur) VALUES(?, ?, ?, ?)");
 	$reponse -> execute(array($valeur_capteur_hex, $date, $numero, $id_utilisateur));
 }
 
