0719bc8b2eb5b782df4af4529dd734a6dc764aed
HomeYann <k2092324@mvrht.com>
2017-02-06 15:50:21 +0100

    Ajout de Newsletteurra
    Je vous la présenterai....

---
+++ homeg4/vue/newslettera.php
@@ -0,0 +1,34 @@
+<?php
+
+    $entete = entete("Newsletter");
+    $aside = asideContact("Newsletter");
+    $contenu = '<div id="form_newsletter">';
+    $contenu .= news_topic();
+    $contenu .= '</div>';
+   
+    
+    $reponse = $bdd->query('SELECT titre, message FROM news ORDER BY ID DESC LIMIT 0, 10');
+
+    
+    $contenu .='<div id= "contenu_message">';
+    while ($donnees = $reponse->fetch())
+    {
+        
+        $contenu .= '<div class="police_titre">';
+        $contenu .= $donnees['titre'];
+        $contenu .= '</div>';
+        $contenu .= '<div class="small_police">';
+        $contenu .= $donnees['message'];
+        $contenu .= '</div>';
+    }
+    $contenu .= '</div>';
+
+    $reponse->closeCursor();
+    $contenu .= '</div>';
+
+    $pied = pied();
+
+    include 'gabarit.php';
+
+
+?>
