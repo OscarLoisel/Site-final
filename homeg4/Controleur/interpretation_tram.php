4a30ebda8f6ca9a226d575ff8a8e03b510611ced
HomeYann <k2092324@mvrht.com>
2017-06-16 15:20:08 +0200

    G4a!!!!
    no one see this

--- homeg4/Controleur/interpretation_tram.php
+++ homeg4/Controleur/interpretation_tram.php
@@ -39,7 +39,7 @@ echo $date;
 
 // 1. Récupérer les données brutes
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL,"http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=0000");
+	curl_setopt($ch, CURLOPT_URL,"http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=004A");
 	curl_setopt($ch, CURLOPT_HEADER, FALSE);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	$data = curl_exec($ch);
